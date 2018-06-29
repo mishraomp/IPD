2e0d9d9bf5d470e97b0f318eea6d7f0a1a9a6165
mishraomp <omprakashmishra3978@gmail.com>
2018-06-29 07:05:34 -0800

    deleted dist and added to gitignore

--- user-service/dist/config/ioc.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-var inversify_1 = require("inversify");
-var user_controller_1 = require("../controller/user-controller");
-exports.iocContainer = new inversify_1.Container();
-exports.iocContainer.bind("IUserController").to(user_controller_1.UserController);
-//# sourceMappingURL=ioc.js.map
--- user-service/dist/middleware/middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncExpressFunction = function (fn) {
-    return function (req, res, next) {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    };
-};
-//# sourceMappingURL=middleware.js.map
